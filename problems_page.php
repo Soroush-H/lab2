d4dbd052899a7cf3cb5c4c032c9c9c2a809ee21c
Soroush Hasanpour <shas@kth.se>
2016-04-01 14:52:05 +0200

    version 4

--- problems_page.php
+++ problems_page.php
@@ -10,47 +10,57 @@
 // Connecting, selecting database
 $dbconn = pg_connect("host=localhost dbname=lab2 user=postgres password=Soroush1994")
     or die('Could not connect: ' . pg_last_error());
+
+
+
+
+
 $courseID = $_GET['CourseID'];
 $group = $_GET['group'];
 
-echo "här är courseid: $courseID";
-echo "här är group: $group";
+echo "Här är din courseid: $courseID";
+echo "Här är din grupp: $group";
 
 
 $CourseIDquery = "SELECT Number FROM Recitation WHERE courseid = '$courseID' AND groupr = '$group'";
 
-echo "test1\n";
+// echo "test1\n";
+
 
 $NumberResult = pg_query($CourseIDquery) or die('Query failed:' . pg_last_error());
-echo "NumberResult: $NumberResult\n";
-$ans = pg_fetch_object($NumberResult);
-echo "ans: $ans\n";
-echo "här är number: $ans";
+// echo "NumberResult: $NumberResult\n";
+$ans = pg_fetch_result($NumberResult, 0, number);
+// // while ($ans = pg_fetch_row($NumberResult)) {
+//echo "$ans";
+// // 	echo "<br/>\n"
+// }
+// echo "ans: $ans\n";
+// echo "här är number: $ans";
 
-// // Performing SQL query
-// $query = "SELECT combinations FROM problems WHERE number = '$Number'";
-// $result = pg_query($query) or die('Query failed: ' . pg_last_error());
+// // // Performing SQL query
+$query = "SELECT combination FROM problems WHERE number = '$ans'";
+$result = pg_query($query) or die('Query failed: ' . pg_last_error());
 
 
 
 
-// // Printing results in HTML
-// while ($arr = pg_fetch_array($result, null, PGSQL_ASSOC))
-// {
-// 	//echo "hej";
-// 	foreach ($arr as $col_value) {
-// 		 echo'<form action="last_page.php">';
-// 		 echo '<form>';
-// 		 echo '<input type="radio" name="combinations" value= $col_value >';
-// 		 echo "$col_value";
-// 		 echo '<br>';
-// 		 echo'<input type="submit" value="Submit">';
-// 		 echo '</form>';
+// Printing results in HTML
+while ($arr = pg_fetch_array($result, null, PGSQL_ASSOC))
+{
+	//echo "hej";
+	foreach ($arr as $col_value) {
+		 echo'<form action="last_page.php">';
+		 echo '<form>';
+		 echo '<input type="radio" name="combinations" value= $col_value >';
+		 echo "$col_value";
+		 echo '<br>';
+		 echo'<input type="submit" value="Submit">';
+		 echo '</form>';
 
    
-// 	}
+	}
 
-// }
+}
 
 
 
