2d94dfa218310632f0329fbc2ad6507209a4dbbc
Soroush Hasanpour <shas@kth.se>
2016-04-01 12:40:12 +0200

    init push

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+// error_reporting(E_ALL);
+//ini_set( 'display_errors','1'); 
+
+
+// Connecting, selecting database
+$dbconn = pg_connect("host=localhost dbname=lab2 user=postgres password=Soroush1994")
+    or die('Could not connect: ' . pg_last_error());
+
+// Performing SQL query
+$query = 'SELECT combination FROM Problems';
+$result = pg_query($query) or die('Query failed: ' . pg_last_error());
+
+
+  echo'<form action="problems_page.php">';
+  echo'First name:<br>';
+  echo '<input type="text" name="firstname" value=""><br>';
+  echo'Last name:<br>';
+  echo'<input type="text" name="lastname" value=""><br><br>';
+  echo'Group (between A and :<br>';
+  echo'<input type="text" name="group" value=""><br><br>';
+  echo'Recitation number:<br>';
+  echo'<input type="text" name="recitation" value=""><br><br>';
+  echo'<input type="submit" value="Submit">';
+  echo'</form>';
+
+// Printing results in HTML
+while ($arr = pg_fetch_array($result, null, PGSQL_ASSOC))
+{
+
+
+	foreach ($arr as $col_value) {
+
+		 /*echo '<form>';
+		 echo '<input type="radio" name="combinations" value= $col_value >';
+		 echo "$col_value";
+		 echo '<br>';
+		 echo '</form>';
+*/   
+
+
+	}
+
+}
+
+
+
+
+
+
+// Free resultset
+pg_free_result($result);
+
+// Closing connection
+pg_close($dbconn);
+?>
+
+</body>
+</html>
---
+++ problems_page.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+// error_reporting(E_ALL);
+//ini_set( 'display_errors','1'); 
+
+
+// Connecting, selecting database
+$dbconn = pg_connect("host=localhost dbname=lab2 user=postgres password=Soroush1994")
+    or die('Could not connect: ' . pg_last_error());
+
+// Performing SQL query
+$query = 'SELECT combination FROM Problems';
+$result = pg_query($query) or die('Query failed: ' . pg_last_error());
+
+
+
+
+// Printing results in HTML
+while ($arr = pg_fetch_array($result, null, PGSQL_ASSOC))
+{
+	//echo "hej";
+	foreach ($arr as $col_value) {
+		 echo'<form action="last_page.php">';
+		 echo '<form>';
+		 echo '<input type="radio" name="combinations" value= $col_value >';
+		 echo "$col_value";
+		 echo '<br>';
+		 echo'<input type="submit" value="Submit">';
+		 echo '</form>';
+
+   
+	}
+
+}
+
+
+
+
+
+
+// Free resultset
+pg_free_result($result);
+
+// Closing connection
+pg_close($dbconn);
+?>
+
+</body>
+</html>
